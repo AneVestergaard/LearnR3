ecd5dfbaaaa606d95b17af6d4941c08ad615dded
Ane Emilie Vestergaard <aneemiliefv@gmail.com>
2026-05-06 12:23:44 +0200

    Make function to get ID

--- R/functions.R
+++ R/functions.R
@@ -17,11 +17,27 @@ read <- function(file_path, max_rows = 100) {
 
 
 read_all <- function(filename) {
-  files <- here::here("data-raw/nurses-stress/") %>%
-    fs::dir_ls(regexp = "HR.csv.gz", recurse = TRUE)
+  files <- here::here("data-raw/nurses-stress/") |>
+    fs::dir_ls(regexp = filename, recurse = TRUE)
 
-  data <- files %>%
-    purrr::map(read) %>%
-    purrr:list_rbind(names_to = "file_path_id")
+  data <- files |>
+    purrr::map(read) |>
+    purrr::list_rbind(names_to = "file_path_id")
+
+  return(data)
+}
+
+
+get_participant_id <- function(data) {
+  data <- data %>%
+    dplyr::mutate(
+      id = stringr::str_extract(
+        file_path_id,
+        pattern = "/stress/[:alnum:]{2}/"
+      ) %>%
+        stringr::str_remove("/stress/") %>% str_remove("/"),
+      .before = file_path_id
+    ) %>%
+    dplyr::select(-file_path_id)
   return(data)
 }
