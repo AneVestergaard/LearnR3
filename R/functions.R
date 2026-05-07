1c69591360716d1f5e73b968dc6ff61ad3552d5b
Ane Emilie Vestergaard <aneemiliefv@gmail.com>
2026-05-07 12:09:50 +0200

    the final dataset

--- R/functions.R
+++ R/functions.R
@@ -5,7 +5,7 @@
 #'
 #' @returns Outputs a dataframe/ tibble
 
-read <- function(file_path, max_rows = 100) {
+read <- function(file_path, max_rows = Inf) {
   data <- file_path |>
     readr::read_csv(
       show_col_types = FALSE,
