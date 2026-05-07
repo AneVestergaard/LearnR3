4dbad590ca53a126232e21fae0950c50d9590648
Ane Emilie Vestergaard <aneemiliefv@gmail.com>
2026-05-07 09:54:00 +0200

    make function to tidy up the dates and times to prepare for pivoting

--- R/functions.R
+++ R/functions.R
@@ -60,3 +60,24 @@ summarise_by_datetime <- function(data) {
     )
   return(summarised_data)
 }
+
+
+#' Tidy up the dates in the survey results
+#'
+#' @param dataframe of the survey results data
+#'
+#' @returns. A data frame
+
+tidy_survey_dates <- function(data) {
+  tidied <- data %>%
+    dplyr::mutate(
+      date = mdy(date),
+      start_datetime = lubridate::as_datetime(paste(date, start_time)),
+      end_datetime = lubridate::as_datetime(paste(date, end_time)),
+      datetime_id = start_datetime,
+      .before = start_time
+    ) %>%
+    dplyr::select(-c(date, start_time, end_time, duration))
+  return(tidied)
+}
+
